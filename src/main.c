4b096bc1c4b42083ddff338128ba33a0f0f2d88e
Katja Toivola <ktoivola@student.hive.fi>
2025-01-14 14:37:09 +0200

    deleted unnecessary source file

--- src/main.c
+++ src/main.c
@@ -6,31 +6,42 @@
 /*   By: ktoivola <ktoivola@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 19:32:07 by ktoivola          #+#    #+#             */
-/*   Updated: 2025/01/13 17:19:11 by ktoivola         ###   ########.fr       */
+/*   Updated: 2025/01/14 14:36:19 by ktoivola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
-#include <stdio.h>
 
-void	ft_game_hook(void *param)
+static void	init_game(t_cub *cub)
 {
-	t_cub	*cub;
-
-	cub = (t_cub *)param;
-    ray_cast(cub);
-	draw_minimap(cub);
+	load_textures(cub);
+	cub->mlx = mlx_init(WIN_X, WIN_Y, "Cub3D", true);
+	if (cub->mlx == NULL)
+		handle_error(mlx_errno);
+	cub->mlx_img = mlx_new_image(cub->mlx, WIN_X, WIN_Y);
+	if (cub->mlx_img == NULL)
+		error_terminate_mlx(cub, mlx_errno);
+	if (mlx_image_to_window(cub->mlx, cub->mlx_img, 0, 0) < 0)
+		error_terminate_mlx(cub, mlx_errno);
+	printf("****Game started****\n");
+	printf("map_width: %zu\n", cub->map_width);
+	printf("player.ppos.x: %f\n", cub->player.ppos.x);
+	printf("player.ppos.y: %f\n", cub->player.ppos.y);
+	printf("player.dir.x: %f\n", cub->player.dir.x);
+	printf("player.dir.y: %f\n", cub->player.dir.y);
+	printf("player.plane_x: %f\n", cub->player.plane_x);
+	printf("player.plane_y: %f\n", cub->player.plane_y);
 }
 
-void	ft_key_hook(void *param)
+static void	ft_key_hook(void *param)
 {
 	t_cub	*cub;
 	mlx_t	*mlx_inst;
-    char    **map;
+	char	**map;
 
 	cub = (t_cub *)param;
 	mlx_inst = cub->mlx;
-    map = cub->map;
+	map = cub->map;
 	if (mlx_is_key_down(mlx_inst, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx_inst);
 	if (mlx_is_key_down(mlx_inst, MLX_KEY_LEFT))
@@ -47,16 +58,25 @@ void	ft_key_hook(void *param)
 		d_key(cub);
 }
 
-void    game_loop(t_cub *cub)
+static void	ft_game_hook(void *param)
 {
-    mlx_loop_hook(cub->mlx, &ft_key_hook, cub);
-    mlx_loop_hook(cub->mlx, &ft_game_hook, cub);
+	t_cub	*cub;
+
+	cub = (t_cub *)param;
+	ray_cast(cub);
+	draw_minimap(cub);
+}
+
+static void	game_loop(t_cub *cub)
+{
+	mlx_loop_hook(cub->mlx, &ft_key_hook, cub);
+	mlx_loop_hook(cub->mlx, &ft_game_hook, cub);
 	mlx_loop(cub->mlx);
 }
 
 int	main(int argc, char **argv)
 {
-	t_cub cub;
+	t_cub	cub;
 
 	init_main_struct(&cub);
 	if (handle_arguments(argc, argv) == 1)
@@ -72,8 +92,7 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	init_game(&cub);
-    game_loop(&cub);
-    free_all_resources(&cub);
+	game_loop(&cub);
+	free_all_resources(&cub);
 	return (EXIT_SUCCESS);
 }
-
